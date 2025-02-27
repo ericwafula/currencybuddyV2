51ee1e972e2a3b1b778faa3ba2df152d9fbf2fae
ericwathome <ericwathome007@gmail.com>
2025-02-27 17:23:25 +0300

    added fonts and Typography configuration

--- core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/Type.kt
+++ core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/Type.kt
@@ -2,35 +2,65 @@ package tech.ericwathome.core.presentation.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography =
     Typography(
         bodyLarge =
             TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = AppFonts.inter,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
                 letterSpacing = 0.5.sp,
             ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-     */
+        bodyMedium =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+                letterSpacing = 0.25.sp,
+            ),
+        bodySmall =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.25.sp,
+            ),
+        titleLarge =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Normal,
+                fontSize = 22.sp,
+                lineHeight = 28.sp,
+                letterSpacing = 0.sp,
+            ),
+        titleMedium =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Normal,
+                fontSize = 18.sp,
+                lineHeight = 24.sp,
+                letterSpacing = 0.sp,
+            ),
+        titleSmall =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                lineHeight = 20.sp,
+                letterSpacing = 0.sp,
+            ),
+        labelSmall =
+            TextStyle(
+                fontFamily = AppFonts.inter,
+                fontWeight = FontWeight.Medium,
+                fontSize = 11.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+            ),
     )
