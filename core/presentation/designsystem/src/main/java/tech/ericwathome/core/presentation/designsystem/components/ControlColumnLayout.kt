d3ac0162ca2c38d6d55a55101d589fe0c41dbc36
ericwathome <ericwathome007@gmail.com>
2025-03-01 12:02:28 +0300

    added control column layout

---
+++ core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/components/ControlColumnLayout.kt
@@ -0,0 +1,50 @@
+package tech.ericwathome.core.presentation.designsystem.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import tech.ericwathome.core.presentation.designsystem.CurrencybuddyTheme
+import tech.ericwathome.core.presentation.designsystem.utils.PreviewLightDarkWithBackground
+
+@Composable
+fun ControlColumnLayout(
+    modifier: Modifier = Modifier,
+    onClickAC: () -> Unit,
+    onClickDEL: () -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        SecondaryButtonOutlined(
+            modifier = Modifier.fillMaxWidth(),
+            text = "AC",
+            onClick = onClickAC,
+        )
+        SecondaryButtonOutlined(
+            modifier = Modifier.fillMaxWidth().height(136.dp),
+            text = "DEL",
+            onClick = onClickDEL,
+        )
+    }
+}
+
+@PreviewLightDarkWithBackground
+@Composable
+private fun ControlColumnLayoutPreview() {
+    CurrencybuddyTheme {
+        Surface {
+            ControlColumnLayout(
+                modifier = Modifier.width(80.dp),
+                onClickAC = {},
+                onClickDEL = {},
+            )
+        }
+    }
+}
