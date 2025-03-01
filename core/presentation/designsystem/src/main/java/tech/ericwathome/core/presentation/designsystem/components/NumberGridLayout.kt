80bb565f6c0d8cc220177c592dc25bc90be56899
ericwathome <ericwathome007@gmail.com>
2025-03-01 11:23:18 +0300

    added number grid layout

---
+++ core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/components/NumberGridLayout.kt
@@ -0,0 +1,40 @@
+package tech.ericwathome.core.presentation.designsystem.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import tech.ericwathome.core.presentation.designsystem.CurrencybuddyTheme
+
+@Composable
+fun NumberGridLayout(
+    modifier: Modifier = Modifier,
+    onClickNumber: (Int) -> Unit = {},
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(3),
+        reverseLayout = true,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items((1..9).toList()) {
+            SecondaryButton(
+                text = it.toString(),
+                onClick = { onClickNumber(it) },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NumberGridPreview() {
+    CurrencybuddyTheme {
+        NumberGridLayout()
+    }
+}
