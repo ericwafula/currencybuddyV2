497efa18c47c56d7cef86c8805a7d1c36770b86c
ericwathome <ericwathome007@gmail.com>
2025-03-01 10:15:25 +0300

    Added outlined secondary button component

---
+++ core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/components/SecondaryButtonOutlined.kt
@@ -0,0 +1,63 @@
+package tech.ericwathome.core.presentation.designsystem.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import tech.ericwathome.core.presentation.designsystem.CurrencybuddyTheme
+import tech.ericwathome.core.presentation.designsystem.utils.LocalTextUtils
+import tech.ericwathome.core.presentation.designsystem.utils.PreviewLightDarkWithBackground
+
+@Composable
+fun SecondaryButtonOutlined(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    text: String,
+) {
+    val textUtils = LocalTextUtils.current
+
+    Box(
+        modifier =
+            modifier
+                .sizeIn(minHeight = 56.dp, minWidth = 56.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.15f),
+                    shape = RoundedCornerShape(12.dp),
+                )
+                .clickable { onClick() },
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            style =
+                MaterialTheme.typography.bodyLarge.copy(
+                    fontSize = with(textUtils) { MaterialTheme.typography.bodyLarge.fontSize.fixedSize },
+                ),
+        )
+    }
+}
+
+@PreviewLightDarkWithBackground
+@Composable
+private fun SecondaryButtonOutlinedPreview() {
+    CurrencybuddyTheme {
+        Surface {
+            SecondaryButtonOutlined(
+                modifier = Modifier,
+                onClick = { },
+                text = "AC",
+            )
+        }
+    }
+}
