858d1e44f6e57583d3f89ec03ed341dcdd96be71
ericwathome <ericwathome007@gmail.com>
2025-03-01 23:53:41 +0300

    added liquid loading animation

---
+++ core/presentation/designsystem/src/main/java/tech/ericwathome/core/presentation/designsystem/components/LiquidLoadingAnimation.kt
@@ -0,0 +1,36 @@
+@file:Keep
+
+package tech.ericwathome.core.presentation.designsystem.components
+
+import androidx.annotation.Keep
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import tech.ericwathome.core.presentation.designsystem.R
+
+@Composable
+fun LiquidLoadingAnimation(
+    modifier: Modifier = Modifier,
+    isPlaying: Boolean = true,
+) {
+    val lottieComposition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.liquid_loading),
+    )
+
+    val progress by animateLottieCompositionAsState(
+        composition = lottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = isPlaying,
+    )
+
+    LottieAnimation(
+        composition = lottieComposition,
+        progress = progress,
+        modifier = modifier,
+    )
+}
