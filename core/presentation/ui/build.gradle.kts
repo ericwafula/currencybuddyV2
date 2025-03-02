8dc997f004ac10da6077edfa7deb216c5c8f42a1
ericwathome <ericwathome007@gmail.com>
2025-03-02 11:51:01 +0300

    added util for open network settings

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -11,6 +11,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.timber)
     implementation(projects.core.domain)
     implementation(projects.core.presentation.designsystem)
 }
