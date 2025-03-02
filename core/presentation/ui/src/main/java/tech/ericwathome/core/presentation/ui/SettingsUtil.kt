d3d3ccab92cc2cfbeceda20a56ea816da98ba1dc
ericwathome <ericwathome007@gmail.com>
2025-03-02 12:00:08 +0300

    added formating and Keep annotation

--- core/presentation/ui/src/main/java/tech/ericwathome/core/presentation/ui/SettingsUtil.kt
+++ core/presentation/ui/src/main/java/tech/ericwathome/core/presentation/ui/SettingsUtil.kt
@@ -1,3 +1,5 @@
+@file:Keep
+
 package tech.ericwathome.core.presentation.ui
 
 import android.app.Activity
@@ -6,6 +8,7 @@ import android.os.Build
 import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Keep
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
@@ -14,19 +17,21 @@ import timber.log.Timber
 @Composable
 fun rememberOpenNetworkSettings(): () -> Unit {
     val context = LocalContext.current
-    val networkSettingsLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        Timber.d("Network settings result: ${result.resultCode}")
-    }
-
-    val settingsIntent = remember {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
-        } else {
-            Intent(Settings.ACTION_WIRELESS_SETTINGS)
+    val networkSettingsLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult(),
+        ) { result ->
+            Timber.d("Network settings result: ${result.resultCode}")
         }
-    }
+
+    val settingsIntent =
+        remember {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+            } else {
+                Intent(Settings.ACTION_WIRELESS_SETTINGS)
+            }
+        }
 
     return {
         if (context is Activity) {
