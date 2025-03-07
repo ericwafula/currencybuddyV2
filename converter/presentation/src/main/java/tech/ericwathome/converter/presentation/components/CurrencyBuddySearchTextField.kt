0b370755f95ef1d677e5a56b14d5012d6bea53b9
ericwathome <ericwathome007@gmail.com>
2025-03-07 10:05:43 +0300

    Closes #81; ft: Fixes~ Create search bar component

---
+++ converter/presentation/src/main/java/tech/ericwathome/converter/presentation/components/CurrencyBuddySearchTextField.kt
@@ -0,0 +1,46 @@
+package tech.ericwathome.converter.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import tech.ericwathome.converter.presentation.R
+import tech.ericwathome.core.presentation.designsystem.CurrencybuddyTheme
+import tech.ericwathome.core.presentation.designsystem.assets.SearchIcon
+import tech.ericwathome.core.presentation.designsystem.components.CurrencyBuddyTextField
+
+@Composable
+fun CurrencyBuddySearchTextField(
+    modifier: Modifier = Modifier,
+    state: TextFieldState,
+) {
+    CurrencyBuddyTextField(
+        modifier = modifier,
+        state = state,
+        hint = stringResource(R.string.search_for_currency),
+        leadingIcon = SearchIcon,
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun CurrencyBuddySearchTextFieldPreview() {
+    CurrencybuddyTheme {
+        Surface {
+            Box(
+                modifier = Modifier.padding(16.dp),
+            ) {
+                CurrencyBuddySearchTextField(
+                    state = TextFieldState(),
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
