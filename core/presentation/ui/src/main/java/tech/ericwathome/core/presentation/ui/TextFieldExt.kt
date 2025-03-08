482a3bcbf6103337b538772e65985ce3241fd693
ericwathome <ericwathome007@gmail.com>
2025-03-08 08:17:34 +0300

    added initial search query functionality

--- core/presentation/ui/src/main/java/tech/ericwathome/core/presentation/ui/TextFieldExt.kt
+++ core/presentation/ui/src/main/java/tech/ericwathome/core/presentation/ui/TextFieldExt.kt
@@ -1,5 +1,8 @@
+@file:Keep
+
 package tech.ericwathome.core.presentation.ui
 
+import androidx.annotation.Keep
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.snapshotFlow
 
