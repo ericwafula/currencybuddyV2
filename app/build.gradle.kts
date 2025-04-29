0aca2e7fd96d7f365735f66b1c8ef3eb31ffcbc5
ericwafula <ericwathome007@gmail.com>
2025-04-29 18:51:31 +0300

    removed defined signing configs to remove the possibility of a certificate chain during app signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
 dependencies {
